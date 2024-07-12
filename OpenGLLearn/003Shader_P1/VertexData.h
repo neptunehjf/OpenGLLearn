ff8346444d4c922b4f3bbd179cb9fce0425fe10c
neptunehjf <neptunehjf@sina.com>
2024-07-12 11:02:22 +0900

    003Shader Practice
    003Shader Practice

---
+++ OpenGLLearn/003Shader_P1/VertexData.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define STRIDE_SIZE 6
+#define POSITION_SIZE 3
+#define COLOR_SIZE 3
+
+float vertex[] =
+{
+	 //Î»ÖÃ               //ÑÕÉ«
+	 0.0f,  0.0f, 0.0f,  1.0f, 0.0f, 0.0f, // 0
+	 0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // 1
+	-0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // 2
+	 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // 3
+	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // 4
+};
+
+unsigned int index[] =
+{
+	0, 1, 2,
+	0, 3, 4
+};
