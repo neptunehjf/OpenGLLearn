3ddf876dba2176cbcbde5a8d10ec84d75d20220a
neptunehjf <neptunehjf@sina.com>
2024-07-21 13:15:51 +0900

    008Color
    008Color

---
+++ OpenGLLearn/008Color/Camera.h
@@ -0,0 +1,87 @@
+#ifndef CAMERA_H  
+#define CAMERA_H
+
+#include "glm/glm.hpp"
+#include "glm//gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+
+using namespace std;
+using namespace glm;
+
+class Camera
+{
+public:
+
+	Camera(const vec3& camPos, const vec3& camFront, const vec3& camUp);
+
+	float camSpeed;
+	vec3 camPos;
+	vec3 camFront;
+	vec3 camUp;
+
+	float deltaTime; // 当前帧与上一帧的时间差
+	float lastFrame; // 上一帧的时间
+	float currentFrame; //当前帧时间
+
+	float lastX;
+	float lastY;
+	bool  isFirst;
+	float pitchValue;
+	float yawValue;
+
+	float fov;
+
+	void setCamView();
+	mat4 getCamView();
+
+	void setCamFov(float fov);
+	float getCamFov();
+
+private:
+	mat4 view;
+};
+
+Camera::Camera(const vec3& camPos, const vec3& camFront, const vec3& camUp)
+{
+	camSpeed = 5.0f;
+
+	deltaTime = 0.0f; 
+	lastFrame = 0.0f;  
+	currentFrame = 0.0f;  
+
+	lastX = 0.0f;
+	lastY = 0.0f;
+	isFirst = true;
+	pitchValue = 0.0f;
+	yawValue = -90.0f; // 默认镜头朝向X正方向，所以向左转90度校正
+
+	fov = 45.0f;
+
+	this->camPos = camPos;
+	this->camFront = camFront;
+	this->camUp = camUp;
+}
+
+void Camera::setCamView()
+{
+	view = lookAt(camPos, camPos + camFront, camUp);
+}
+
+mat4 Camera::getCamView()
+{
+	return view;
+}
+
+void Camera::setCamFov(float fov)
+{
+	this->fov = fov;
+}
+
+float Camera::getCamFov()
+{
+	return fov;
+}
+
+#endif // !CAMERA_H
+
