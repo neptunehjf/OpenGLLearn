628666228476a97cc5e6058e334ff42aaf895e7e
neptunehjf <neptunehjf@sina.com>
2024-07-24 07:25:40 +0900

    009BasicLighing practice3
    改为从view空间计算冯氏光照

--- OpenGLLearn/009BasicLighting_Practice/shader.fs
+++ OpenGLLearn/009BasicLighting_Practice/shader.fs
@@ -1,13 +1,11 @@
 #version 330 core
 
 in vec3 fragPos;
-in vec3 normalModel;
-in vec3 normalView;
+in vec3 normal;
+in vec3 lightPos;
 
 uniform vec3 uni_objectColor;
 uniform vec3 uni_lightColor;
-uniform vec3 uni_lightPos;
-uniform vec3 uni_viewPos;
 
 uniform float uni_ambientStrength;
 uniform float uni_diffuseStrength;
@@ -22,13 +20,13 @@ void main()
 	vec3 ambient = uni_ambientStrength * uni_lightColor;
 	
 	// 漫反射光照diffuse
-	vec3 norm = normalize(normalModel);
-	vec3 lightDir = normalize(uni_lightPos - fragPos);
+	vec3 norm = normalize(normal);
+	vec3 lightDir = normalize(lightPos - fragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = uni_diffuseStrength * diff * uni_lightColor;
 	
 	// 镜面光照specular
-	vec3 viewDir = normalize(uni_viewPos - fragPos);
+	vec3 viewDir = normalize(vec3(0.0, 0.0, 0.0) - fragPos);
 	vec3 reflectDir = normalize(reflect(-lightDir, norm));
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), uni_specularFactor);
 	vec3 specular = uni_specularStrength * spec * uni_lightColor;
--- OpenGLLearn/009BasicLighting_Practice/shader.vs
+++ OpenGLLearn/009BasicLighting_Practice/shader.vs
@@ -6,14 +6,18 @@ layout(location = 1) in vec3 aNormal;
 uniform mat4 uni_model;
 uniform mat4 uni_view;
 uniform mat4 uni_projection;
+uniform vec3 uni_lightPos;
 
 out vec3 fragPos;
-out vec3 normalModel;
+out vec3 normal;
+out vec3 lightPos;
 
 void main()
 {
   gl_Position = uni_projection * uni_view * uni_model * vec4(aPos, 1.0);  // 注意矩阵变换的顺序是从右向左
 
-  fragPos = vec3(uni_model * vec4(aPos, 1.0));
-  normalModel = mat3(transpose(inverse(uni_model))) * aNormal;
+  fragPos = vec3(uni_view * vec4(aPos, 1.0));
+  normal = mat3(transpose(inverse(uni_view))) * aNormal; //transpose(inverse(uni_view) 此操作在GPU开销大，实际开发时尽可能放到CPU处理
+  lightPos = vec3(uni_view * vec4(uni_lightPos, 1.0));
+  //lightPos = mat3(uni_view) * uni_lightPos;  // 只有在法线矩阵可以在计算前用mat4把w齐次向量截掉，因为法线只有方向特性，没有位置特性。但是这里光源位置也截掉就错了
 }
