daefb0676c8aa3d8b7eae7ddab9d944c0e3fb7a3
neptunehjf <neptunehjf@sina.com>
2024-07-26 12:43:59 +0900

    012LightCaster 新建工程
    012LightCaster 新建工程

---
+++ OpenGLLearn/012LightCaster/shader.fs
@@ -0,0 +1,48 @@
+#version 330 core
+
+in vec3 fragPos;
+in vec3 normal;
+in vec2 texCoord;
+
+uniform vec3 uni_viewPos;
+
+struct Material
+{
+	sampler2D diffuse;
+	sampler2D specular;
+	int shininess;
+};
+
+struct Light
+{
+	vec3 lightPos;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+
+out vec4 fragColor;
+
+void main()
+{
+	// 环境光照ambient
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, texCoord));
+	
+	// 漫反射光照diffuse
+	vec3 norm = normalize(normal);
+	vec3 lightDir = normalize(light.lightPos - fragPos);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * light.diffuse * vec3(texture(material.diffuse, texCoord));
+	
+	// 镜面光照specular
+	vec3 viewDir = normalize(uni_viewPos - fragPos);
+	vec3 reflectDir = normalize(reflect(-lightDir, norm));
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = spec * light.specular * vec3(texture(material.specular, texCoord));
+
+	// 光照颜色与物体颜色混合，营造光照效果
+	fragColor = vec4(ambient + diffuse + specular, 1.0);
+}
