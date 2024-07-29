0f61afd57f1e8a133328ee309671aa2ce60c2b7b
neptunehjf <neptunehjf@sina.com>
2024-07-29 17:12:46 +0900

    014Assimp 删除多余的箱子
    删除多余的箱子

--- OpenGLLearn/014Assimp/assimp.cpp
+++ OpenGLLearn/014Assimp/assimp.cpp
@@ -29,21 +29,8 @@
 using namespace std;
 using namespace glm;
 
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
-
-glm::vec3 cubePosition[] = {
-	glm::vec3(0.0f,  0.0f,  0.0f),
-	glm::vec3(2.0f,  5.0f, -15.0f),
-	glm::vec3(-1.5f, -2.2f, -2.5f),
-	glm::vec3(-3.8f, -2.0f, -12.3f),
-	glm::vec3(2.4f, -0.4f, -3.5f),
-	glm::vec3(-1.7f,  3.0f, -7.5f),
-	glm::vec3(1.3f, -2.0f, -2.5f),
-	glm::vec3(1.5f,  2.0f, -2.5f),
-	glm::vec3(1.5f,  0.2f, -1.5f),
-	glm::vec3(-1.3f,  1.0f, -1.5f)
-};
+#define WINDOW_WIDTH 2560
+#define WINDOW_HEIGHT 1440
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -353,15 +340,11 @@ int main()
 		myShader.SetMat4("uni_projection", projection);
 
 		// model矩阵 local -> world
-		for (unsigned int i = 0; i < 10; i++)
-		{
-			mat4 model = mat4(1.0f); // mat4初始化最好显示调用初始化为单位矩阵，因为新版本mat4 model可能是全0矩阵
-			model = translate(model, cubePosition[i]);
-			float angle = 20.0f * i;
-			model = rotate(model, radians(angle), vec3(1.0f, 0.3f, 0.5f));
-			myShader.SetMat4("uni_model", model);
-			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-		}
+
+		mat4 model = mat4(1.0f); // mat4初始化最好显示调用初始化为单位矩阵，因为新版本mat4 model可能是全0矩阵
+		myShader.SetMat4("uni_model", model);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
 
 		// 解绑
 		glBindVertexArray(0);
