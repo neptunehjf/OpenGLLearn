8877244792e1fe776a066cd9b413cc61ad4106d2
neptunehjf <neptunehjf@sina.com>
2024-09-11 10:13:15 +0900

    030PointShadow 新建工程

---
+++ OpenGLLearn/030PointShadow/Common.h
@@ -0,0 +1,78 @@
+#pragma once
+
+using namespace std;
+using namespace glm;
+
+
+#define WINDOW_WIDTH 2560
+#define WINDOW_HEIGHT 1440
+
+#define CHARACTRER_SCALE_DEFAULT 0.1f
+#define CHARACTRER_SCALE_OUTLINE 0.103f
+#define CUBE_SCALE_DEFAULT 1.0f
+#define CUBE_SCALE_OUTLINE 1.05f
+
+//之前1万个小行星帧数144，但是10万个小行星帧数只有20多帧了(3080 + i7 10700k + 16G)。
+//研究发现，本场景中，片段着色器对帧数影响小，优化空间约等于0；面剔除对帧数的影响也基本为0.
+//最后发现几何着色器会把图元拆解成顶点，每个顶点都会吊一次片段着色器，最后又要把顶点装配成图元，这里的开销是很大的。
+//删除几何着色器后帧数达到了120多帧，优化成功。
+#define ROCK_NUM 1000
+
+#define MSAA_SAMPLE_NUM 4
+
+#define ROTATE_SPEED_PLANET 0.1f
+#define ROTATE_SPEED_ROCK 0.1f
+
+#define SHADOW_RESOLUTION_WIDTH 1024
+#define SHADOW_RESOLUTION_HEIGHT 1024
+
+double preTime = 0.0;
+double deltaTime = 0.0;
+double curTime = 0.0;
+
+const vec3 lampPos[4] = { vec3(20.0f, 3.0f, 0.0f) , vec3(20.0f, 3.0f, -10.0f) , vec3(20.0f, 3.0f, -20.0f) , vec3(20.0f, 3.0f, -30.0f) };
+
+float windowWidth = WINDOW_WIDTH;
+float windowHeight = WINDOW_HEIGHT;
+
+/************** Imgui变量 **************/
+//float posValue = 0.0f;
+vec3 bkgColor = vec3(0.0f, 0.0f, 0.0f);
+vec3 dirLight_direction = vec3(-5.0f, -5.0f, -5.0f);
+vec3 dirLight_ambient = vec3(0.0f);
+vec3 dirLight_diffuse = vec3(1.0f);
+vec3 dirLight_specular = vec3(0.0f);
+vec3 pointLight_ambient = vec3(0.0f);
+vec3 pointLight_diffuse = vec3(0.0f);
+vec3 pointLight_specular = vec3(0.0f);
+vec3 spotLight_ambient = vec3(0.0f);
+vec3 spotLight_diffuse = vec3(0.0f);
+vec3 spotLight_specular = vec3(0.0f);
+float spotLight_innerCos = 5.0f;
+float spotLight_outerCos = 8.0f;
+int item = 0;
+int material_shininess = 256;
+int postProcessType = 0;
+float sampleOffsetBase = 300.0f;
+float imgui_speed = 10.0f;
+float imgui_camNear = 0.1f;
+float imgui_camFar = 500.0f;
+float pointSize = 1.0f;
+bool bSplitScreen = 0;
+bool bGMTest = 0;
+float explodeMag = 0.0;
+bool bBlending = true;
+bool bFaceCulling = false;
+float normalLen = 0.1;
+bool bInstanceTest = 0;
+bool bSkyBox = 1;
+bool bMSAA = true;
+int iLightModel = 1; //{ "Phong", "Blinn-Phong" }
+bool bGammaCorrection = true;
+int iAtteFormula = 0;
+bool bShadow = true;
+bool bDisDepthmap = false;
+bool bBias = true;
+bool bFrontFaceCulling = false;
+/************** Imgui变量 **************/
+
---
+++ OpenGLLearn/030PointShadow/ShaderLighting.gs
@@ -0,0 +1,69 @@
+#version 330 core
+
+layout (triangles) in;
+layout (triangle_strip, max_vertices = 3) out;
+
+in VS_OUT
+{
+	vec3 fragPos;
+	vec3 normal;         //  既可以直接用顶点数据的normal，也可以在几何着色器计算normal。之前理解错了
+	vec2 texCoord;
+    vec4 position;
+    mat4 view;
+	mat4 projection;	
+    vec4 fragPosLightSpace;
+} gs_in[];
+
+out GS_OUT
+{
+	vec3 fragPos;
+	vec3 normal;
+	vec2 texCoord;
+    vec4 fragPosLightSpace;
+} gs_out;
+
+uniform float magnitude;
+
+vec3 GetNormal()
+{
+    vec3 a = vec3(gs_in[0].position - gs_in[1].position);
+    vec3 b = vec3(gs_in[2].position - gs_in[1].position);
+
+    return -normalize(cross(a, b));
+}
+
+vec4 explode(vec4 position, vec3 normal)
+{
+    vec3 direction = magnitude * normal;
+    return position + vec4(direction, 0.0);
+}
+
+void main() 
+{    
+    //vec3 normal = GetNormal();                 
+    vec3 normal = normalize(gs_in[1].normal);
+
+    // 如果涉及到在特定坐标空间进行计算的，要确保在对应的空间计算后，然后再进行剩下的矩阵变换（view projection之类的）
+    gl_Position = gs_in[0].projection * gs_in[0].view * explode(gs_in[0].position, normal);    // 1
+    gs_out.fragPos = gs_in[0].fragPos;
+    gs_out.normal = gs_in[0].normal;
+    gs_out.texCoord = gs_in[0].texCoord;
+    gs_out.fragPosLightSpace = gs_in[0].fragPosLightSpace;
+    EmitVertex();   
+
+    gl_Position = gs_in[1].projection * gs_in[1].view * explode(gs_in[1].position, normal);    // 2
+    gs_out.fragPos = gs_in[1].fragPos;
+    gs_out.normal = gs_in[1].normal;
+    gs_out.texCoord = gs_in[1].texCoord;
+    gs_out.fragPosLightSpace = gs_in[1].fragPosLightSpace;
+    EmitVertex();
+
+    gl_Position = gs_in[2].projection * gs_in[2].view * explode(gs_in[2].position, normal);    // 3
+    gs_out.fragPos = gs_in[2].fragPos;
+    gs_out.normal = gs_in[2].normal;
+    gs_out.texCoord = gs_in[2].texCoord;
+    gs_out.fragPosLightSpace = gs_in[2].fragPosLightSpace;
+    EmitVertex();
+
+    EndPrimitive();    
+}
