8d4df9c51780678ad8e7c07da975c988efb47274
neptunehjf <neptunehjf@sina.com>
2024-11-24 13:04:44 +0900

    039PBR_IBL_Specular 重要性采样的算法代码
    只是算法，还没有实装测试代码

--- OpenGLLearn/039PBR_IBL_Specular/PBR.fs
+++ OpenGLLearn/039PBR_IBL_Specular/PBR.fs
@@ -22,6 +22,9 @@ uniform vec3 camPos;
 
 const float PI = 3.14159265359;
 // ----------------------------------------------------------------------------
+// N H决定了不同位置的片段的微表面与H一致的概率，概率越高的片段，反射越强
+// roughness 决定了整体微表面的方向的随机程度，roughness越大，反射与无反射区域的过度越平滑
+// 但是diffuse的部分并没有用到概率，感觉只是一个加强版的blin-phong光照，真正要用到概率的是在specular的NDF部分
 float DistributionGGX(vec3 N, vec3 H, float roughness)
 {
     float a = roughness*roughness;
@@ -62,6 +65,55 @@ vec3 fresnelSchlick(float cosTheta, vec3 F0)
     return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
 }
 // ----------------------------------------------------------------------------
+
+// Van Der Corput 序列 生成的仍然是随机样本，但样本分布更均匀，返回值是归一化到0到1之间的小数
+float RadicalInverse_VdC(uint bits) 
+{
+    bits = (bits << 16u) | (bits >> 16u);
+    bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
+    bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
+    bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
+    bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
+    return float(bits) * 2.3283064365386963e-10; // / 0x100000000
+}
+// ----------------------------------------------------------------------------
+
+// Hammersley 以Van Der Corput 序列为基础，构造了一组二维随机样本
+vec2 Hammersley(uint i, uint N)
+{
+    return vec2(float(i)/float(N), RadicalInverse_VdC(i));
+}
+
+// 重要性采样，根据Hammersley二维随机样本得出一个符合目标pdf（主要集中在N附近，并受到roughness影响）的H向量
+vec3 ImportanceSampleGGX(vec2 Xi, vec3 N, float roughness)
+{
+    // 从Hammersley分布得到对应的Tangent空间的球面坐标
+    // phi由Xi.x(Hammersley采样下标)得到，与概率无关
+    float phi = 2 * PI * Xi.x;
+
+    // theta由Xi.y(Hammersley采样随机值) 和 roughness得到
+    // roughness越大，theta越大，也就是H与N的夹角越大，这个与材质有关，与概率无关
+    // Xi.y(Hammersley采样随机值)越大，theta越小，也就是H与N的夹角越小，可见概率高的都是夹角小的，这就是重要性采样
+    // 此公式的推理比较复杂，有空再研究一下
+    float theta = acos(sqrt((1.0 - Xi.y) / (1.0 + (roughness * roughness - 1.0) * Xi.y)));
+
+    // 球面坐标 --》笛卡尔坐标
+    vec3 H;
+    H.x = sin(theta) * cos(phi);
+    H.y = sin(theta) * sin(phi);
+    H.z = cos(theta);
+
+    // 切线坐标 --》世界坐标
+    vec3 up = vec3(0.0, 1.0, 0.0);
+    vec3 tangent = normalize(cross(up, N));
+    vec3 bitangent = normalize(cross(N, tangent));
+    mat3 TBN = mat3(tangent, bitangent, N);
+
+    vec3 sampleVec = normalize(TBN * H);
+
+    return sampleVec;
+}
+
 void main()
 {		
     vec3 N = normalize(Normal);
