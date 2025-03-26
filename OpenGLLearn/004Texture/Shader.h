58791437dfef1cec827a2e2d98e713ea1888c13b
neptunehjf <neptunehjf@sina.com>
2025-03-26 17:26:13 +0900

    修正と日本語の注釈

--- OpenGLLearn/004Texture/Shader.h
+++ OpenGLLearn/004Texture/Shader.h
@@ -1,5 +1,10 @@
-// Shader����ʧ��ʱ��ֻ��ӡ��Ϣ�ǲ����� --> ������ɫ������ID��ֵ���ж� ��ID��0��ʾ��Ч
-// ��װ��set����������ɫʧ�� --> API�ô���
+﻿// debug经验
+// Shader构造失败时，只打印信息是不够的 --> 根据着色器程序ID的值来判断 ，ID是0表示无效
+// 封装的set函数设置颜色失败 --> API用错了
+// デバッグ経験
+// シェーダー構築失敗時の処理不備 → シェーダープログラムIDの値(0)で有効性判定が必要
+// カプセル化したset関数が色を設置失敗 → APIの誤用
+
 
 #ifndef SHADER_H  
 #define SHADER_H
@@ -20,19 +25,22 @@ using namespace std;
 class Shader
 {
 public:
-	//����ID 
+	//程序ID 
+	//プログラムID
 	GLuint ID = 0; 
 
 	Shader(const char* vertexShaderPath, const char* fragmentShaderPath);
 
-	//ʹ��/�������
+	//使用/激活程序
+	// プログラムの使用/有効化
 	bool Use();
 
-	//ɾ��Shader����
+	//删除Shader程序
+	// シェーダープログラムを削除する
 	void Remove();
 
-	//uniform���ߺ���
-
+	//uniform工具函数
+	// uniformユーティリティ関数
 	void SetBool(const string &name, bool value) const;
 	void SetInt(const string& name, int value) const;
 	void SetFloat(const string& name, float value) const;
@@ -42,27 +50,33 @@ public:
 
 Shader::Shader(const char* vertexShaderPath, const char* fragmentShaderPath)
 {
-	// 1.��Ӳ�̶�ȡshaderԴ��
+	// 1.从硬盘读取shader源码
+	// 1. ハードディスクからシェーダーソースコードを読み込む
 	string vertexCode;
 	string fragmentCode;
 	ifstream vShaderFile;
 	ifstream fShaderFile;
-	// �����׳��쳣����
+	// 设置抛出异常类型
+	// スロー例外型の指定
 	vShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
 	fShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
 	try
 	{
-		// ���ļ�
+		// 打开文件
+		// ファイルを開く
 		vShaderFile.open(vertexShaderPath);
 		fShaderFile.open(fragmentShaderPath);
-		// ��ȡ��������
+		// 读取到数据流
+		// データストリームに読み込む
 		stringstream vShaderStream, fShaderStream;
 		vShaderStream << vShaderFile.rdbuf();
 		fShaderStream << fShaderFile.rdbuf();
-		// �ر��ļ���
+		// 关闭文件流
+		// ファイルストリームを閉じる
 		vShaderFile.close();
 		fShaderFile.close();
-		// ��������ȡ��str
+		// 从数据流取出str
+		// データストリームから文字列を取得
 		vertexCode = vShaderStream.str();
 		fragmentCode = fShaderStream.str();
 	}
@@ -73,17 +87,19 @@ Shader::Shader(const char* vertexShaderPath, const char* fragmentShaderPath)
 		cerr << "Error code: " << e.code() << endl;
 	}
 
-	// ��ȡshaderԴ��
+	// 获取shader源码
+	// シェーダーソースコードを取得
 	const char* vertexShaderSource = vertexCode.c_str();
 	const char* fragmentShaderSource = fragmentCode.c_str();
 
 	int success = 0;
 	char infoLog[LOG_LENGTH] = "\0";
 
-	// ����shader����
+	// 编译shader代码
+	// シェーダーコードをコンパイル
 	GLuint vertexShader = 0;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);  // ������ֱ���� &vertexCode.c_str()������ ��ΪvertexCode.c_str()������ֵ��
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);  // 在这里直接用 &vertexCode.c_str()报错， 因为vertexCode.c_str()不是左值。
 	glCompileShader(vertexShader);
 
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -109,7 +125,8 @@ Shader::Shader(const char* vertexShaderPath, const char* fragmentShaderPath)
 		cout << "Fragment shader compile failed!\n" << infoLog << endl;
 	}
 
-	// ������ɫ������
+	// 链接着色器程序
+	// シェーダープログラムをリンク
 	ID = glCreateProgram();
 	if (ID == 0)
 	{
@@ -128,7 +145,8 @@ Shader::Shader(const char* vertexShaderPath, const char* fragmentShaderPath)
 		glGetProgramInfoLog(ID, sizeof(infoLog), NULL, infoLog);
 		cout << "Shader program link failed!\n" << infoLog << endl;
 
-		// ����ʧ�ܣ�shader����Ӧ����Ϊ������
+		// 连接失败，shader程序应该置为不可用
+		// リンク失敗時、シェーダープログラムを無効状態に設定
 		ID = 0;
 	}
 
